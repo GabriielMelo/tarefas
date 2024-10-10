d1eb2f5c295e975c1c66a8c82c38afc02620969e
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2024-10-10 10:34:07 -0300

    feat: added first home configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-
+import Image from "next/image";
+import logo from "../../public/images/logo.png";
+import styles from "./page.module.css";
 export default function Home() {
   return (
-    <main>
-      <h1>teste</h1>
+    <main className={styles.container}>
+      <section className={styles.logoContent}>
+        <Image
+          src={logo}
+          alt="logo"
+          priority={true}
+          quality={100}
+          className={styles.logo}
+        />
+        <h1 className={styles.title}>
+          Sistema feito para voce organizar <br />
+          seus estudos e tarefas
+        </h1>
+        <div className={styles.tagContent}>
+          <p className={styles.tag}>0 Posts</p>
+          <p className={styles.tag}>0 Comments</p>
+        </div>
+      </section>
     </main>
   );
 }
